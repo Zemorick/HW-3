ef2e204ac22be898a27c94958c9ca31ddbb9f7b6
Andy Lin <46833884+Zemorick@users.noreply.github.com>
2020-03-04 17:52:39 -0500

    Add files via upload

---
+++ HW3/HW3/src/LinkedList.java
@@ -0,0 +1,57 @@
+public class LinkedList implements ListInterface {
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void add(Comparable element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean add(Comparable element, int index) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void addSorted(Comparable element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Comparable get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Comparable replace(Comparable element, int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean remove(int index) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void removeAll() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
